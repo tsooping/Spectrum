96572154caad864ebc4b8228cb9659b070b56cfa
tsooping <info.tansooping@gmail.com>
2024-07-06 18:01:43 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
+
+function App() {
+  const [count, setCofd32sunfddsdfddfffdfdt] = useState(1);
+
+  return (
+    <BackgroundGradientAnimation>
+      <div className="grid grid-cols-2">
+        <div>
+          <h1>dfws</h1>
+        </div>
+        <div className="bg-white rounded-md"></div>
+      </div>
+    </BackgroundGradientAnimation>
+  );
+}
+
+export default App;
